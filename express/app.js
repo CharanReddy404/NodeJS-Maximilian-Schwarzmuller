17ee8a2a7c3036306cea21b62a7ddbfef18d9fc0
charanreddy404 <charanlucky6143@gmail.com>
2023-01-03 08:30:51 +0530

    learning express

---
+++ express/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Server started at PORT 3000...');
+});
