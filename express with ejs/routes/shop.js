b0d60d42329e51418957c886dabd6ac3dc98c8c5
charanreddy404 <charanlucky6143@gmail.com>
2023-01-04 22:39:24 +0530

    express with ejs

---
+++ express with ejs/routes/shop.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const routes = express.Router();
+
+// const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+routes.get('/', (req, res, next) => {
+  //   res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'My Shop', path: '/' });
+});
+
+module.exports = routes;
