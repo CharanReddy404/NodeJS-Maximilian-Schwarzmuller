4b4556a49d8025e8bd6ff940bba6f9a438722dd5
charanreddy404 <charanlucky6143@gmail.com>
2023-01-09 20:21:11 +0530

    nodejs with SQL completed

--- NodeJs complete gide - MySQL/controllers/shop.js
+++ NodeJs complete gide - MySQL/controllers/shop.js
@@ -154,23 +154,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render('shop/checkout', {
+//     path: '/checkout',
+//     pageTitle: 'Checkout',
+//   });
+// };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -183,7 +191,10 @@ exports.postOrder = (req, res, next) => {
         );
       });
     })
-    .then((results) => {
+    .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect('/orders');
     })
     .catch((err) => console.log(err));
