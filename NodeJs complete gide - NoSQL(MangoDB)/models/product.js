80a4ee767cae62b0fc7680c3253bb19c81899937
charanreddy404 <charanlucky6143@gmail.com>
2023-01-11 01:26:21 +0530

    fixed edit and delete product functionality

--- NodeJs complete gide - NoSQL(MangoDB)/models/product.js
+++ NodeJs complete gide - NoSQL(MangoDB)/models/product.js
@@ -1,19 +1,26 @@
-const mangodb = require('mongodb');
+const mongodb = require('mongodb');
 const { getDb } = require('../util/database');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -27,7 +34,6 @@ class Product {
       .find()
       .toArray()
       .then((products) => {
-        console.log(products);
         return products;
       })
       .catch((err) => console.log(err));
@@ -37,11 +43,21 @@ class Product {
     const db = getDb();
     return db
       .collection('products')
-      .find({ _id: new mangodb.ObjectId(prodId) })
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then((product) => {
-        console.log(product);
         return product;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then(() => {
+        console.log('Producted Deleted');
       })
       .catch((err) => console.log(err));
   }
