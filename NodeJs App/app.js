afe1f7d3a4b1defa5f00472d1fdcc0a4a713e161
charanreddy404 <charanlucky6143@gmail.com>
2023-01-14 22:56:17 +0530

    updated

--- NodeJs App/app.js
+++ NodeJs App/app.js
@@ -36,6 +36,14 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  User.findById('63c0b7da17a7ca2830364893')
+    .then((user) => {
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
